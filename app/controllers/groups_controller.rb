c6f368df904a0a65791ab4cdd4d9785b95e7541b
songhao <songhao@songhaodeMacBook-Pro.local>
2017-04-02 15:48:41 +0800

    show group's creator info

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -18,10 +18,10 @@ class GroupsController < ApplicationController
 
   def create
   @group = Group.new(group_params)
-  @group = current_user
-  
+  @group.user = current_user
+
   if @group.save
-    redirect_to group_path
+    redirect_to groups_path
   else
     render :new
   end
